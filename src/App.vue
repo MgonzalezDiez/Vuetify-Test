98d01728d80271ea3f4a565ec18664e224c88910
Marco Antonio González <mgonzalezdiez@gmail.com>
2020-09-03 23:01:52 -0400

    Uso de vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="info" dark>
+      <v-toolbar-title>Edutecno DashBoard</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text to="/home" width="90px" class="mr-2">Home</v-btn>
+      <v-btn text to="/">Login</v-btn>
+    </v-app-bar>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+    <v-card height="150">
+      <v-footer absolute class="font-weight-medium" color="info">
+        <v-col class="text-center" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-footer>
+    </v-card>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: 'App',
+};
+</script>
